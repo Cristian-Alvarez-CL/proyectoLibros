fb399dee4128a2d192dd3ccfda775e06af576f90
CAP <cap@cap.com>
2021-02-08 22:13:40 -0300

    Se regulzariza error de visualizacion de login

--- src/js/component/tablaInfo.js
+++ src/js/component/tablaInfo.js
@@ -169,7 +169,7 @@ const DatatablePage = () => {
       <section className="page-section" id="tabla">
         <div className="container">
           <div className="text-center">
-              <h2 className="section-heading text-uppercase">Filtro de Usuarios</h2>
+              <h2 className="section-heading text-uppercase">Mis Publicaciones</h2>
               <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
           </div>
           <MDBDataTable
