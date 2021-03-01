12f1f142efef46d5502851f33470b0ede37b4b43
Duglas Medina <duglasmedina@gmail.com>
2021-03-01 15:41:45 -0300

    ajuste de archivos

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -90,22 +90,7 @@ const Navbar = (props) => {
               <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                 {store.datosPerfil ? (
                   <a className="dropdown-item font-weight-bold m-1 p-1">
-                    <svg
-                      aria-hidden="true"
-                      focusable="false"
-                      data-prefix="far"
-                      data-icon="user"
-                      className="svg-inline--fa fa-user fa-w-14 mr-1"
-                      role="img"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 448 512"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M313.6 304c-28.7 0-42.5 16-89.6 16-47.1 0-60.8-16-89.6-16C60.2 304 0 364.2 0 438.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-25.6c0-74.2-60.2-134.4-134.4-134.4zM400 464H48v-25.6c0-47.6 38.8-86.4 86.4-86.4 14.6 0 38.3 16 89.6 16 51.7 0 74.9-16 89.6-16 47.6 0 86.4 38.8 86.4 86.4V464zM224 288c79.5 0 144-64.5 144-144S303.5 0 224 0 80 64.5 80 144s64.5 144 144 144zm0-240c52.9 0 96 43.1 96 96s-43.1 96-96 96-96-43.1-96-96 43.1-96 96-96z"
-                      ></path>
-                    </svg>
-                     {store.datosPerfil.nombreCompleto}
+                    Bienvenido: {store.datosPerfil.nombreCompleto}
                   </a>
                 ) : (
                   <Link
@@ -120,7 +105,7 @@ const Navbar = (props) => {
                 )}
                 {store.isAuth ? (
                   <Link className="dropdown-item" to="/misPublicaciones">
-                    Mis Publicaciones
+                      Mis Publicaciones
                   </Link>
                 ) : null}
                 {store.isAuth ? (
